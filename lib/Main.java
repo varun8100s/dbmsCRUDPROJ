74e57a9cc4782acc0e137924e4bcbadf33ad17e3
setAccount <varunraveesh@gmail.com>
2022-11-20 02:33:02 +0530

    configured sql server

--- lib/Main.java
+++ lib/Main.java
@@ -2,16 +2,15 @@ import java.sql.*;
 
 class Main {
     public static void main(String args[]) {
+        String url="jdbc:mysql://sql12.freesqldatabase.com:3306/sql12579100", 
+        password="xyScnZuvUQ", username="sql12579100";
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            // ------------------STEPS-------------------------
-            // start apache and mysql in xampp
-            // "localhost/phpmyadmin" in browser to add, modify database
-            // create a database, replace "databaseName" with database name in the url
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/databaseName", "root", "");
+           
+            Connection con = DriverManager.getConnection(url, username, password);
 
             Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("select * from emp");
+            ResultSet rs = stmt.executeQuery("select * from test");//test-table name
             while (rs.next())
                 System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3));
             con.close();
